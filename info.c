5cd45e27c17311a72ccf8f2d63e4a9c924bf1d7d
bigyan mahara <bigyanmahara7@gmail.com>
2025-04-29 09:43:56 +0545

    Commited

---
+++ info.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main () {
+
+printf ("Name: Bigyan Mahara")
+printf ("Address: Tarkeshwar" )
+printf ("College: SWSC, basundhara, kathmandu ")
+printf ("department: BCA")
+printf ("Enrollment: 2nd semester")
+printf ("level: Bachelor")
+return 0;
+
+
+}
+
+
+
