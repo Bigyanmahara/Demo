910397e1665c7637a03a59f4c08a549530c9ba6e
bigyan mahara <bigyanmahara7@gmail.com>
2025-05-04 17:34:49 +0545

    commited on april 5 by bigyan

---
+++ addnumber.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main () {
+int firstnumber,secondnumber; //variable initialization
+printf("enter the firstnumber""/n");
+scanf("%d",&firstnumber);
+
+printf("enter the secondnumber");
+scanf ("%d,&secondnumber");
+printf("/n");
+
+//operation or equation;
+printf("result = first number + second number");//display
+printf("addition=%d");
+
+return 0;
+}
